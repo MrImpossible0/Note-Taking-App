295dd1d79e35b5c3996ebb761d918b255feceb1d
Nick <Nick@NICKSLAPTOP>
2022-02-17 22:14:06 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_Taking_App
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Messages"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 235;
+        }
+
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            textTitle.Clear();
+            textMessage.Clear();
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(textTitle.Text, textMessage.Text);
+
+            textTitle.Clear();
+            textMessage.Clear();
+        }
+
+        private void bttRead_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if (index > -1)
+            {
+                textTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                textMessage.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        private void bttDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+        }
+    }
+}
